dbdf186de21236b56d4549d071128fd237732783
KH Jahnavi Padma Priya <khjahnavipadmapriya23@gmail.com>
2026-04-11 16:14:31 +0530

    Time: 0 ms (100%), Space: 42.5 MB (97.03%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public ListNode middleNode(ListNode head) {
+
+        int count = 0;
+        ListNode temp = head;
+
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        temp = head;
+
+        for (int i = 0; i < count / 2; i++) {
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+}
