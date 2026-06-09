207b14feed2805c81ef66706faf18d03a9f9168b
KH Jahnavi Padma Priya <khjahnavipadmapriya23@gmail.com>
2026-06-09 23:21:40 +0530

    Time: 0 ms (100%), Space: 42.7 MB (16.68%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int res =0;
+        int powOf5 = 5;
+        while(n>=powOf5){
+            res = res + (n/powOf5);
+            powOf5 = powOf5 * 5;
+        }
+    return res;
+    }
+}
