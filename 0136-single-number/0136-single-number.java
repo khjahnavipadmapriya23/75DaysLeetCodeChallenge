460d4e7188b55b93290df14cc95047a9cbcb8c8f
KH Jahnavi Padma Priya <khjahnavipadmapriya23@gmail.com>
2026-06-23 16:12:45 +0530

    Time: 1 ms (99.97%), Space: 47.1 MB (13.86%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,15 +1,13 @@
-import java.util.*;
-
 class Solution {
-    public int singleNumber(int[] nums){
-        HashSet<Integer> set = new HashSet<>();
-        for(int num: nums){
-            if(set.contains(num))
-                set.remove(num);
-            else{
-                set.add(num);
-            }
+
+    public int singleNumber(int[] nums) {
+
+        int result = 0;
+
+        for (int num : nums) {
+            result ^= num;
         }
-        return set.iterator().next();
+
+        return result;
     }
 }
