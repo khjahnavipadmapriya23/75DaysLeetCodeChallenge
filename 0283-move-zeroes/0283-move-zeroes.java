9da6d5337ca18fd8a2b9a0ef0d1bc9908a4bfe2e
KH Jahnavi Padma Priya <khjahnavipadmapriya23@gmail.com>
2026-06-24 14:14:52 +0530

    Time: 2 ms (91.99%), Space: 47.2 MB (97.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,15 @@
 import java.util.*;
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=0){
-                nums[i] = nums[j] ;
-                i++;
+        int position = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[position] = nums[i];
+                position++;
             }
         }
-        for(;i<nums.length;i++){
-            nums[i]=0;
+        for(int i=position;i<nums.length;i++){
+            nums[i] = 0;
         }
-        System.out.println(Arrays.toString(nums));
     }
 }
