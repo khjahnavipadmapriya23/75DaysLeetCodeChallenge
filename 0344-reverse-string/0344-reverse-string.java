148b52a10ee537b2570e9ad4ca9cc590813a936b
KH Jahnavi Padma Priya <khjahnavipadmapriya23@gmail.com>
2026-08-04 17:04:43 +0530

    Time: 0 ms (100%), Space: 48 MB (88.94%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -2,7 +2,7 @@ class Solution {
     public void reverseString(char[] s) {
         int left = 0;
         int right = s.length-1;
-        while(left<=right){
+        while(left<right){
             if(s[left]!=s[right]){
                 char temp = s[left];
                 s[left] = s[right];
