98a16ebd78b76942f9599798952ab893e2687323
KH Jahnavi Padma Priya <khjahnavipadmapriya23@gmail.com>
2026-08-13 22:32:31 +0530

    Time: 31 ms (38.93%), Space: 46.9 MB (57.98%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -4,9 +4,9 @@ class Solution {
         for(char ch : s.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        char[] ch = s.toCharArray();
-        for(int i =0;i<ch.length;i++) {
-            if(map.get(ch[i])==1) 
+        
+        for(int i =0;i<s.length();i++) {
+            if(map.get(s.charAt(i))==1) 
                 return i;
         }
 
