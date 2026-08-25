4505d444e74cf0618b7de9eee9ea5c23f3cbb14c
KH Jahnavi Padma Priya <khjahnavipadmapriya23@gmail.com>
2026-08-25 06:40:52 +0530

    Time: 2 ms (70.98%), Space: 55.8 MB (31.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,14 @@
-import java.util.*;
 class Solution {
-    public int majorityElement(int[] nums){
-       int candidate = 0;
-       int count = 0;
-       for(int num : nums){
-        if(count==0) {
-            candidate = num;
-            count++;
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(count==0) candidate = nums[i];
+            if(nums[i]==candidate){
+                count++;
+            }
+            else count--;
         }
-        else if(num==candidate) count++;
-        else count--;
-       }
-       return candidate;
+        return candidate;
     }
 }
